33163cac51fb7b1a0461112c4c7ffbc9e536690c
zaidmansuri958 <firozam196@gmail.com>
2023-02-16 00:38:58 +0530

    Food Recipe

--- app/src/main/java/com/zaidmansuri/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/zaidmansuri/foodrecipe/MainActivity.kt
@@ -2,6 +2,8 @@ package com.zaidmansuri.foodrecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         val bnv=findViewById<BottomNavigationView>(R.id.bnv)
         val navController=Navigation.findNavController(this,R.id.fragment)
 
