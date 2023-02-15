d498355286f15b3874bf8cd604c0d042e28c29d1
zaidmansuri958 <firozam196@gmail.com>
2023-02-15 22:04:38 +0530

    Food Reciep

--- app/src/main/java/com/zaidmansuri/foodrecipe/fragments/CatagoryFragment.kt
+++ app/src/main/java/com/zaidmansuri/foodrecipe/fragments/CatagoryFragment.kt
@@ -5,20 +5,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zaidmansuri.foodrecipe.Adapter.CategoryAdapter
+import com.zaidmansuri.foodrecipe.Model.Category
 import com.zaidmansuri.foodrecipe.R
+import com.zaidmansuri.foodrecipe.ViewModel.HomeViewModel
+import com.zaidmansuri.foodrecipe.databinding.FragmentCategoryBinding
+import kotlinx.coroutines.launch
 
 class CatagoryFragment : Fragment() {
+    private lateinit var binding: FragmentCategoryBinding
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var categoryAdapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        categoryAdapter = CategoryAdapter()
+
 
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.getCategory().observe(viewLifecycleOwner) { categoryList ->
+                categoryAdapter.setCategoryList(categoryList = categoryList as ArrayList<Category>)
+                binding.categoryFragmentRecycle.apply {
+                    layoutManager = GridLayoutManager(context,3)
+                    adapter = categoryAdapter
+                }
+            }
+        }
+
     }
 
 }
